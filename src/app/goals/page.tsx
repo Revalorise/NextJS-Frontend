46e8aa5465bf4daae7d3576804b5dcf6371df5fa
Bon <Blackball007b@gmail.com>
2025-01-03 13:47:36 +0700

    added alert component and main /goals page

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,3 +1,41 @@
+'use client';
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import AlertTab from "@/components/global_components/alert"
+
 export default function Home() {
-    return <h1>What</h1>
+    return (
+        <div className="md:container md:mx-auto pt-5">
+
+            <AlertTab />
+
+            <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>Is it styled?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It comes with default styles that matches the other
+                        components&apos; aesthetic.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger>Is it animated?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It's animated by default, but you can disable it if you prefer.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    )
 }
---
+++ src/components/global_components/alert.tsx
@@ -0,0 +1,20 @@
+import { Terminal } from "lucide-react"
+
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
+
+export default function AlertTab() {
+    return (
+        <Alert>
+            <Terminal className="h-4 w-4"/>
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>
+                You can add components and dependencies to your app using the cli.
+            </AlertDescription>
+        </Alert>
+    )
+};
